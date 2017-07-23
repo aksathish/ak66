ffe4e8a88b7c31469dceff1c608a9d058363a0a0
aksathish <30342289+aksathish@users.noreply.github.com>
2017-07-23 16:36:35 +0530

    Update and rename primeornot.c to primeornotttt.c

--- primeornotttt.c
+++ primeornotttt.c
@@ -1,20 +1,23 @@
 #include <stdio.h> 
-main() 
+int main() 
 {
 int n, i, c = 0;
 printf("Enter any number n:");
 scanf("%d", &n);
-for (i = 1; i <= n; i++) {
-if (n % i == 0) {
+for (i = 1; i <= n; i++) 
+{
+if (n % i == 0) 
+{
 c++;
 }
 }
-if (c == 2) {
+if (c == 2) 
+{
 printf("n is a Prime number");
 }
-else {
+else 
+{
 printf("n is not a Prime number");
-}
-return 0;    
+}    
 }
 
